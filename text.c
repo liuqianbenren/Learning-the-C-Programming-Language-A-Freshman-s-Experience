fb513cc48ce886b08420b60f10fff597e7cc6f5f
liuqianbenren <liuqian20070222@qq.com>
2025-10-24 21:52:13 +0800

    Add files via upload
    just a small text,but spent me 2 hours ,at the last i found several bugs with the help of AI .

---
+++ text.c
@@ -0,0 +1,233 @@
+#include<stdio.h>
+#include<string.h>
+
+int wordnumber(char arr[], char word)
+{
+    int number = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        if (arr[i] == word)
+        {
+            number++;
+        }
+    }
+    return number;
+}
+
+int main(void)
+{
+
+    char str[1000];
+    scanf("%s", str);
+    
+    int a1 = wordnumber(str, 'a');  
+    int b1 = wordnumber(str, 'b');
+    int c1 = wordnumber(str, 'c');
+    int d1 = wordnumber(str, 'd');
+    int e1 = wordnumber(str, 'e');
+    int f1 = wordnumber(str, 'f');
+    int g1 = wordnumber(str, 'g');
+    int h1 = wordnumber(str, 'h');
+    int i1 = wordnumber(str, 'i');
+    int j1 = wordnumber(str, 'j');
+    int k1 = wordnumber(str, 'k');
+    int l1 = wordnumber(str, 'l');
+    int m1 = wordnumber(str, 'm');
+    int n1 = wordnumber(str, 'n');
+    int o1 = wordnumber(str, 'o');
+    int p1 = wordnumber(str, 'p');
+    int q1 = wordnumber(str, 'q');
+    int r1 = wordnumber(str, 'r');
+    int s1 = wordnumber(str, 's');
+    int t1 = wordnumber(str, 't');
+    int u1 = wordnumber(str, 'u');
+    int v1 = wordnumber(str, 'v');
+    int w1 = wordnumber(str, 'w');
+    int x1 = wordnumber(str, 'x');  
+    int y1 = wordnumber(str, 'y');
+    int z1 = wordnumber(str, 'z');
+    
+    printf("a");
+    while(a1 > 0) {
+        printf("#");
+        a1--;
+    }
+    printf("\n");
+    
+    printf("b");
+    while(b1 > 0) {
+        printf("#");
+        b1--;
+    }
+    printf("\n");
+    
+    printf("c");
+    while(c1 > 0) {
+        printf("#");
+        c1--;
+    }
+    printf("\n");
+    
+    printf("d");
+    while(d1 > 0) {
+        printf("#");
+        d1--;
+    }
+    printf("\n");
+    
+    printf("e");
+    while(e1 > 0) {
+        printf("#");
+        e1--;
+    }
+    printf("\n");
+    
+    printf("f");
+    while(f1 > 0) {
+        printf("#");
+        f1--;
+    }
+    printf("\n");
+    
+    printf("g");
+    while(g1 > 0) {
+        printf("#");
+        g1--;
+    }
+    printf("\n");
+    
+    printf("h");
+    while(h1 > 0) {
+        printf("#");
+        h1--;
+    }
+    printf("\n");
+    
+    printf("i");
+    while(i1 > 0) {
+        printf("#");
+        i1--;
+    }
+    printf("\n");
+    
+    printf("j");
+    while(j1 > 0) {
+        printf("#");
+        j1--;
+    }
+    printf("\n");
+    
+    printf("k");
+    while(k1 > 0) {
+        printf("#");
+        k1--;
+    }
+    printf("\n");
+    
+    printf("l");
+    while(l1 > 0) {
+        printf("#");
+        l1--;
+    }
+    printf("\n");
+    
+    printf("m");
+    while(m1 > 0) {
+        printf("#");
+        m1--;
+    }
+    printf("\n");  
+    
+    printf("n");
+    while(n1 > 0) {
+        printf("#");
+        n1--;
+    }
+    printf("\n");
+    
+    printf("o");
+    while(o1 > 0) {
+        printf("#");
+        o1--;
+    }
+    printf("\n");
+    
+    printf("p");
+    while(p1 > 0) {
+        printf("#");
+        p1--;
+    }
+    printf("\n");
+    
+    printf("q");
+    while(q1 > 0) {
+        printf("#");
+        q1--;
+    }
+    printf("\n");
+    
+    printf("r");
+    while(r1 > 0) {
+        printf("#");
+        r1--;
+    }
+    printf("\n");
+    
+    printf("s");
+    while(s1 > 0) {
+        printf("#");
+        s1--;
+    }
+    printf("\n");
+    
+    printf("t");
+    while(t1 > 0) {
+        printf("#");
+        t1--;
+    }
+    printf("\n");
+    
+    printf("u");
+    while(u1 > 0) {
+        printf("#");
+        u1--;
+    }
+    printf("\n");
+    
+    printf("v");
+    while(v1 > 0) {
+        printf("#");
+        v1--;
+    }
+    printf("\n");
+    
+    printf("w");
+    while(w1 > 0) {
+        printf("#");
+        w1--;
+    }
+    printf("\n");
+    
+    printf("x");
+    while(x1 > 0) {
+        printf("#");
+        x1--;
+    }
+    printf("\n");
+    
+    printf("y");
+    while(y1 > 0) {
+        printf("#");
+        y1--;
+    }
+    printf("\n");
+    
+    printf("z");
+    while(z1 > 0) {
+        printf("#");
+        z1--;
+    }
+    printf("\n");
+
+    return 0;
+}
